bdeee0e96c178d79db5bf16ea3f4755a1142ba45
Seçil <45385894+ssecill@users.noreply.github.com>
2021-09-08 12:23:06 +0300

    Create scaleup.js

---
+++ scaleup.js
@@ -0,0 +1,45 @@
+console.log('Loading event');
+var aws = require('aws-sdk');
+
+exports.handler = function(event, contex, callback) {
+    var ecsService = ['service', 'secill'];
+    var ecsRegion = 'eu-west-1';
+    var ecsCluster = 'secil'
+    var maxCount = 3;
+
+    for (var i = 0; i < ecsService.length; i++) {
+        var ecs = new aws.ECS({
+            region: ecsRegion
+        });
+        ecs.describeServices({
+            services: [ecsService[i]],
+            cluster: ecsCluster
+        }, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                var desiredCount = data.services[0].desiredCount;
+                console.log(desiredCount);
+                for (var i = 0; i < ecsService.length; i++) {
+                    if (desiredCount < maxCount) {
+                        desiredCount++;
+                        var params = {
+                            cluster: ecsCluster,
+                            service: ecsService[i],
+                            desiredCount: maxCount
+                        };
+                        ecs.updateService(params, function(err, data) {
+                            if (err) {
+                                console.log(err, err.stack);
+                            } else {
+                                console.log(data);
+                            }
+                        });
+                    } else {
+                        console.log('mevcutta maximum countta, guncellenmedi!');
+                    }
+                }
+            }
+        });
+    }
+};
